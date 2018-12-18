2ef148639904fe0bf70c46461fe4740bb70a7d19
John Boyington <mjcb@ksu.edu>
2018-12-18 15:59:26 -0600

    added script to plot integrated responses

--- scripts/plot_irs.py
+++ scripts/plot_irs.py
@@ -9,3 +9,15 @@ from driver import Filter_Job
 with open('btube_ir_data.p', 'rb') as F:
     ir = pickle.load(F)
 
+
+def plot_sat_act():
+    """Plots the saturated activities of the integral responses."""
+
+    for key, val in ir.items():
+        if 'l1' in key and 'Cd' not in key:
+            spec = val[1][:, 0]
+            print(spec)
+            plt.semilogy(spec)
+
+
+plot_sat_act()
